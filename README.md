d950cc8216f9461405c183afa4de66774637209c
Amrani <x544d3v@gmail.com>
2020-09-19 03:05:01 -0700

    [IN-DEV] Updated Code .

--- README.md
+++ README.md
@@ -8,6 +8,13 @@
 
 **Status :** Currently working on it . 
 
+**Changelogs :**
+
+```
+[09/06/2020] -> Done Frontend
+[09/06/2020] -> Started Backend ...
+```
+
 >Notes:
 
 + This Repl uses jQuery , rest is made from scratch .
--- js/p4th3r.js
+++ js/p4th3r.js
@@ -15,14 +15,27 @@ class Point
 }
 
 
-let lastLoc = new Point(0, 0)
-let isDragging = false
+const startPT = "imgs/s.png";
+const endPt   = "imgs/e.png";
+const walls   = "imgs/w.png";
+const xImg    = "imgs/x.png";
 
+
+let WriteLog_emptied = false;
+
+let isDragging    = false;
+let end_start_sat = false;
+let get_back_ico  = [false , ""];
 let UNITS   = []
+
 let infos   = {
+
+  "img":startPT,
   "s":null,
   "e":null,
+  "o":[]
 }
+
 
 class UNIT
 {
@@ -52,7 +65,7 @@ class UNIT
       this._v = -1;
 
       // this.c => css class
-      this._c = point.x.toString()+":"+point.y.toString()
+      this._c = point.x * point.y 
     }
 
     get_cords = () => this._p;
@@ -61,13 +74,34 @@ class UNIT
 
 }
 
+function Reset() 
+{
+  WriteLog_emptied  = false;
+  isDragging        = false;
+  end_start_sat     = false;
+  get_back_ico      = [false , ""];
+  UNITS             = []
+
+  infos["img"]      = startPT;
+  infos["s"]        = null;
+  infos["e"]        = null;
+  infos["o"]        = [];
+
+  $('#map').empty();
+  $('#logs').empty();
+
+  __init__();
+
+}
+
+
 function DrawMap() 
 {
     c = 0
     for (var i = 1; i <= 20; i++) for (var j = 1; j <= 60 ; j++) 
     {
       // Writing actual HTML.
-      $('#map').append('<div class="'+c+'" ></div>');
+      $('#map').append('<div _val= "-1" tabindex="'+c+1+'" class="'+c+'" ></div>');
       
       // Storing each UNIT
       UNITS.push(new UNIT(new Point(i, j)))
@@ -76,17 +110,170 @@ function DrawMap()
 }
 
 
+// var WriteLogs = function (target, message, index, interval) {   
+//   if (index < message.length) {
+//     $(target).append(message[index++]);
+//     setTimeout(function () { WriteLogs(target, message, index, interval); }, interval);
+//   }
+// }
+
+
+var WriteLogs = function ( target , message , color, index , interval) 
+{
+  if (!WriteLog_emptied)
+  { 
+    $(target).empty(); 
+    $(target).css("color" , color)
+    WriteLog_emptied = true;
+  }
+
+  if (index < message.length) 
+  {
+    $(target).append(message[index++]);
+    setTimeout(function () { WriteLogs(target, message, color, index, interval); }, interval);
+  }
+  else WriteLog_emptied = false;
+  
+}
+
+
+// out Initialisations to call it manually on Reset
+function __init__() 
+{
+  $(function(){
+    WriteLogs("#logs", "[#] THANK YOU FOR VISITING US_", "yellow" , 0, 50);
+  }); 
+
+  // We Draw First
+  DrawMap();
+
+  // Handle the start Btn click event
+  $("#startBtn").on('click' , () => 
+  {
+    if (!end_start_sat) 
+      $(() => { $("#logs").empty(); WriteLogs("#logs", "[ERROR]  PLEASE SPECIFY  SP / EP !", "red", 0, 50)});
+    else
+    {
+
+      $(() => WriteLogs("#logs", "[#]  START IS NOT IMPLEMENTED YET !", "yellowgreen", 0, 50));
+
+    }
+
+  });
+
+  $("#map").mouseleave((e) => 
+  {
+    $("#map").mouseup()
+  });
+
+  $("#map div").mouseover((e) => 
+  {
+    e.target.focus();
+ 
+      if (e.target != infos['s'] && e.target != infos['e']) 
+      $("."+e.target.className).css("background-image", "url('"+infos['img']+"')")
+
+      else  
+      {
+        get_back_ico = [true , $("."+e.target.className).css("background-image")]
+        $("."+e.target.className).css("background-image", "url('"+xImg+"')")
+      }  
+    
+    
+
+  });
+
+
+  $("#map div").mouseout((e) => 
+  {
+    if (get_back_ico[0] == true) 
+    {
+      $("."+e.target.className).css("background-image", get_back_ico[1])
+      get_back_ico = [false, ""]
+    }
+    else
+    {
+      if ($("."+e.target.className).attr("_val") == "-1") 
+      { 
+        e.target.focus();
+        $("."+e.target.className).css("background-image", "none")
+      }
+    }
+
+  });
+
+
+  $("#map div").mousedown((e) => 
+  {
+    // know that we are on click so if mousemove triggered we can draw
+    isDragging = true;
+
+    const cls = e.target
+
+    // Check if its not an x.png
+    if (!get_back_ico[0]) 
+    {
+    
+      if (infos['s'] == null) 
+      {
+        infos['s'] = cls;
+        infos['img'] = endPt
+        $("."+cls.className).css("background-image", "url('"+startPT+"')")
+        $("."+cls.className).attr("_val" , "0")
+      }
+      else if (infos['e'] == null) 
+      {
+        infos['e'] = cls;
+        infos['img'] = walls
+        $("."+cls.className).css("background-image", "url('"+endPt+"')")
+        $("."+cls.className).attr("_val" , "1")
+        end_start_sat = true
+      }
+    }
+    else
+    {
+      $("#logs").html("");
+      
+      $(function(){
+        WriteLogs("#logs", "[ERROR]  THIS CELL IS ALREADY PRESERVED !", "red", 0, 50);
+      });        
+    }
+  });
+
+
+  $("#map div").mouseup((e) => 
+  {
+    // no more can draw walls
+    isDragging = false;
+  });
+
+
+  $("#map div").mousemove((e) =>
+  {
+    // draw walls
+    if (isDragging && end_start_sat && e.target != infos['e'] && e.target != infos['s']) 
+    {
+      $("."+e.target.className).css("background-image", "url('"+infos['img']+"')")
+      $("."+e.target.className).attr("_val" , "2")
+      UNITS[parseInt($("."+e.target.className).attr("tabindex"))-1]._v=2
+    }
+  });
+
+}
+
+
+function Start_1() 
+{
+  for (var i = 1; i <= 60; i++)
+  {
+    console.log(UNITS[i])
+  }  
+}
+
+
+// builtin anonymous ready function
 $(document).ready(function()
 {
-    // We Draw First
-    DrawMap();
- 
-
-    $("#map div").mousedown((e) => 
-    {
-      let cords = e.target.className
-      console.log(UNITS[cords])
-    });
-
+  __init__()
 
 });
